079b618dd81f18b9e1e2d4f2faf8bf8a763ddec0
xossrifat <151685749+xossrifat@users.noreply.github.com>
2025-04-25 14:25:46 +0600

    Add files via upload

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,33 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+
+firebase.initializeApp({
+    apiKey: 'FIREBASE_API_KEY',
+    authDomain: 'FIREBASE_AUTH_DOMAIN',
+    projectId: 'FIREBASE_PROJECT_ID',
+    storageBucket: 'FIREBASE_STORAGE_BUCKET',
+    messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
+    appId: 'FIREBASE_APP_ID',
+    measurementId: 'FIREBASE_MEASUREMENT_ID',
+});
+
+const messaging = firebase.messaging();
+
+try {
+    messaging.setBackgroundMessageHandler(function (payload) {
+        let data = payload?.notification;
+        const notificationTitle = data?.title;
+        const notificationOptions = {
+            body: data?.body,
+            icon: './logo.png' || 0,
+            image: data?.image
+        };
+
+        return self.registration.showNotification(notificationTitle,
+            notificationOptions);
+    });
+
+} catch (error) {
+    console.log("This is an error ->", error);
+}
